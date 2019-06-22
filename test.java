bf910f5e53e6f661f162f6cf7590806ff6d93363
PhiS <926151+Phi-S@users.noreply.github.com>
2019-06-22 14:43:06 +0200

    rename

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class test {
 
 	public static void main(String[] args) {
 
-		jsch_shell_java_api c = new jsch_shell_java_api("hostname_or_ip", 22, "root", "123");
+		jsch_shell c = new jsch_shell("hostname_or_ip", 22, "root", "123");
 	
 		System.out.println(c.getErg(c.sendCommand("ls /var")));
 		System.out.println(c.getErg(c.sendCommand("ls /etc")));
